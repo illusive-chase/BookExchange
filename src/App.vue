03a09eb2803e4f025814ac962f794467a55cdc8b
illusive-chase <ye_kai@pku.edu.cn>
2023-12-01 20:07:57 +0800

    add element plus

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,23 @@
-<script setup>
-</script>
-
 <template>
-  <div class="text-center">
-    Hello World!
-  </div>
+  <el-config-provider :size="size" :z-index="zIndex">
+    <div class="my-3 text-center">Center</div>
+  </el-config-provider>
+  <i-ep-menu></i-ep-menu>
 </template>
+
+<script>
+import { defineComponent } from 'vue'
+import { ElConfigProvider } from 'element-plus'
+
+export default defineComponent({
+  components: {
+    ElConfigProvider,
+  },
+  setup() {
+    return {
+      zIndex: 3000,
+      size: 'small',
+    }
+  },
+})
+</script>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,35 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
 import UnoCSS from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),UnoCSS()],
+  plugins: [
+    vue(),
+    UnoCSS(),
+    AutoImport({
+      resolvers: [
+        ElementPlusResolver(),
+        IconsResolver({
+          prefix: 'icon'
+        })
+      ],
+    }),
+    Components({
+      resolvers: [
+        ElementPlusResolver(),
+        IconsResolver({
+          enabledCollections: ['ep']
+        })
+      ],
+    }),
+    Icons({
+      autoInstall: true
+    })
+  ],
 })
